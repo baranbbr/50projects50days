a97ff972cdb583287f05b3124ee89d7a4316816c
Baran <baranbaburisme@hotmail.com>
2020-12-06 16:12:57 +0000

    polished p3

--- project3-rotating-nav/script.js
+++ project3-rotating-nav/script.js
@@ -2,7 +2,6 @@ const open = document.getElementById('open')
 const close = document.getElementById('close')
 const container = document.querySelector('.container')
 
-console.log('open')
 open.addEventListener('click', () => {
 	container.classList.add('show-nav')
 })
