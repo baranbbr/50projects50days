955483896edd94945aa10bd89224d9b50e3dd821
8aran <baranbaburisme@hotmail.com>
2021-11-16 02:46:37 +0000

    fixed gif api

--- project40/script.js
+++ project40/script.js
@@ -4,28 +4,30 @@ const btn = document.getElementById('btn')
 btn.addEventListener('click', () => boxesContainer.classList.toggle('big'))
 
 async function createBoxes() {
-    const img = await getRandomGIF()
-    for (let i = 0; i < 4; i++) {
-        for (let j = 0; j < 4; j++) {
-            const box = document.createElement('div')
-            box.classList.add('box')
-            console.log(`img is ${img}`)
-            box.style.backgroundImage = `url(${img})`
-            box.style.backgroundPosition = `${-125 * j}px ${-125 * i}px`
-            boxesContainer.appendChild(box)
-        }
-    }
+	const img = await getRandomGIF()
+	for (let i = 0; i < 4; i++) {
+		for (let j = 0; j < 4; j++) {
+			const box = document.createElement('div')
+			box.classList.add('box')
+			console.log(`img is ${img}`)
+			box.style.backgroundImage = `url(${img})`
+			box.style.backgroundPosition = `${-125 * j}px ${-125 * i}px`
+			boxesContainer.appendChild(box)
+		}
+	}
 }
 
 async function getRandomGIF() {
-    const url = 'https://api.giphy.com/v1/gifs/random?'
-    const API_KEY = 'api_key=F2MgsylcEUeF4Ef5CQLP2UyNhihxtal0'
-    const { data } = await axios.get(`${url}${API_KEY}&tag=positive`)
-    // console.log(data)
-    const imgHeight = data.data.image_height
-    const imgWidth = data.data.image_width
-    // console.log(imgHeight, imgWidth)
-    return data.data.image_url
+	const url = 'https://api.giphy.com/v1/gifs/random?'
+	const API_KEY = 'api_key=F2MgsylcEUeF4Ef5CQLP2UyNhihxtal0'
+	const { data } = await axios.get(`${url}${API_KEY}&tag=happy`)
+	console.log(data)
+	const imgHeight = data.data.images.original.height
+	const imgWidth = data.data.image_width
+	if (imgHeight <= 500) {
+		getRandomGIF()
+	}
+	return data.data.images.original.url
 }
 
 createBoxes()
